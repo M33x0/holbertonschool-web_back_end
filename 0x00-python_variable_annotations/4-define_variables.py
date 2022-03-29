5baf8a00b657c5de1a94e924afae51bcd233c1e3
M33x0 <michael.mbea@gmail.com>
2022-03-29 13:47:15 -0700

    annotation variables

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 """  Python Annotation """
 
-
+"""
+a:int
+pi:float
+is_understand_annotations:boolean
+school:string
+"""
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
