339d303c60f76d03cf3df037f745622500839fba
M33x0 <michael.mbea@gmail.com>
2022-03-29 16:14:23 -0700

    sum of a list of float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""  Python Annotation """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """retur sum of float element in a list"""
+
+    return sum(input_list)
