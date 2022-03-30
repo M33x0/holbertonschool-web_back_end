6747acad4f7ac61f5ee0aacfa0dfadf7da4dd08b
M33x0 <michael.mbea@gmail.com>
2022-03-30 15:49:28 -0700

    complex types

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""  Python Annotation """
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """return some shit"""
+
+    return (k, v * v)
