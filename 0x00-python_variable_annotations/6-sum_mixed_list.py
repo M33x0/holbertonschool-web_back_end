0f96abbe2671c0300b129d0e6225ab8c269d9665
M33x0 <michael.mbea@gmail.com>
2022-03-30 12:19:32 -0700

    added bracket to function synthaxe

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """  Python Annotation """
 
-from typing import Union
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """return sum list of integer and float to float"""
 
     return sum(mxd_lst)
