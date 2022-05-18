b4b244bd7a1fd4b80983a40df079220e4ede12cf
M33x0 <michael.mbea@gmail.com>
2022-05-18 11:29:33 +0100

    async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Async Comprehension """
+
+import random
+import asyncio
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ generator of random """
+
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
